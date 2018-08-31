d5e6c803d29fef7eb02b20d087ece1458a1dde15
Unni <unni@soupedup.com>
2018-08-31 13:14:48 +0530

    Till lession 30

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  template: './server.component.html'
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
+  serverId : number = 10;
+  serverStatus : string = 'offline'
+  getServerStatus () {
+    return this.serverStatus
+  }
 }
